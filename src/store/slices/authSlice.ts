42ca851b3c4ff0a594a969c0959b5097de082460
sidhdharth dhorajiya <99072949+dhorajiya9898@users.noreply.github.com>
2025-08-27 19:37:23 +0530

    Update authSlice.ts
    ok

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://ecommerce-gwz9.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://ecommerce-7f1v.onrender.com/api';
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Configure axios defaults
